e1b80f41f66e9cd9528935819812caa36e70cc7f
zilencers <91873611+zilencers@users.noreply.github.com>
2022-06-30 18:08:00 -0500

    Added performance tweaks

--- setup.sh
+++ setup.sh
@@ -1,64 +1,125 @@
 #!/bin/bash
 
-execute() {
-    echo "Disabling ICMP broadcast echo activity"
-    sysctl -w net.ipv4.icmp_echo_ignore_broadcasts=1
-    echo ""
+SYSCTL_CONF="/etc/sysctl.d/sysctl.conf"
 
-    echo "Disabling ICMP routing redirects"
-    sysctl -w net.ipv4.conf.all.accept_redirects=0
-    sysctl -w net.ipv4.conf.all.shared_media=1
-    sysctl -w net.ipv4.conf.all.secure_redirects=1
-    sysctl -w net.ipv6.conf.all.accept_redirects=0
-    sysctl -w net.ipv4.conf.all.send_redirects=0
-    sysctl -w net.ipv6.conf.all.send_redirects=0
-    echo ""
+file_exists() {
+    if [ ! -f $SYSCTL_CONF ] ; then
+        touch $SYSCTL_CONF
+    fi
+}
 
-    echo "Enforcing sanity checking, ingress filtering or egress filtering"
-    sysctl -w net.ipv4.conf.all.rp_filter=1
-    echo ""
+perf_tweaks() {
+    echo "The following kernel parameters MAY help to increase network performance"
+    printf "Continue (y/N): "
+    read answer
 
-    echo "Logging and dropping 'Martian' packets"
-    sysctl -w net.ipv4.conf.all.log_martians=1
-    echo ""
+    if [ "$answer" == "y" ] ; then
+         echo "Increasing the size of the receiving queue"
+	 echo "net.core.netdev_max_backlog = 16384" >> $SYSCTL_CONF
+         echo ""
 
-    echo "Increase resiliance under heavy TCP load (SYN Flood attacks)"
-    sysctl -w net.ipv4.tcp_max_syn_backlog=1280
-    sysctl -w net.ipv4.tcp_syncookies=1
-    sysctl -w net.ipv4.tcp_fin_timeout=3
-    echo ""
+	 echo "Increasing performance for high speed large file transfer"
+         echo "net.core.rmem_default = 1048576" >> $SYSCTL_CONF
+         echo "net.core.rmem_max = 16777216" >> $SYSCTL_CONF
+         echo "net.core.wmem_default = 1048576" >> $SYSCTL_CONF
+         echo "net.core.wmem_max = 16777216" >> $SYSCTL_CONF
+         echo "net.core.optmem_max = 65536" >> $SYSCTL_CONF
+         echo "net.ipv4.tcp_rmem = 4096 1048576 2097152" >> $SYSCTL_CONF
+         echo "net.ipv4.tcp_wmem = 4096 65536 16777216" >> $SYSCTL_CONF
+         echo ""
+
+	 echo "Increase the default UDP limits"
+         echo "net.ipv4.udp_rmem_min = 8192" >> $SYSCTL_CONF
+         echo "net.ipv4.udp_wmem_min = 8192" >> $SYSCTL_CONF
+         echo ""
+
+	 echo "Enable TCP Fast Open"
+	 echo "net.ipv4.tcp_fastopen = 3" >> $SYSCTL_CONF
+	 echo ""
+
+         echo "This setting kills persistent single connection performance"
+	 echo "net.ipv4.tcp_slow_start_after_idle = 0" >> $SYSCTL_CONF
+	 echo ""
+
+	 echo "Enable MTU probing, longer MTU is better for performace"
+	 echo "net.ipv4.tcp_mtu_probing = 1" >> $SYSCTL_CONF
+         echo""
+    fi
+
+    sysctl -p
 
     echo "Process Complete... "
     echo "Press any key to continue"
     read
 }
 
+sec_tweaks() {
+    echo "Disabling ICMP broadcast echo activity"
+    echo "net.ipv4.icmp_echo_ignore_broadcasts=1" >> $SYSCTL_CONF
+    echo ""
+
+    echo "Disabling ICMP routing redirects"
+    echo "net.ipv4.conf.all.accept_redirects=0" >> $SYSCTL_CONF
+    echo "net.ipv4.conf.default.accept_redirects=0" >> $SYSCTL_CONF
+    echo "net.ipv4.conf.all.secure_redirects=0" >> $SYSCTL_CONF
+    echo "net.ipv4.conf.all.send_redirects=0" >> $SYSCTL_CONF
+    echo "net.ipv6.conf.all.accept_redirects=0" >> $SYSCTL_CONF
+    echo "net.ipv6.conf.default.accept_redirects=0" >> $SYSCTL_CONF
+    echo "net.ipv6.conf.all.send_redirects=0" >> $SYSCTL_CONF
+
+    #  Indicates that the media is shared with different subnets
+    echo "net.ipv4.conf.all.shared_media=1" >> $SYSCTL_CONF
+    echo ""
+    
+    echo "Enforcing sanity checking, protect against IP spoofing"
+    echo "net.ipv4.conf.default.rp_filter=1" >> $SYSCTL_CONF
+    echo "net.ipv4.conf.all.rp_filter=1" >> $SYSCTL_CONF
+    echo ""
+
+    echo "Logging and dropping 'Martian' packets"
+    echo "net.ipv4.conf.default.log_martians=1" >> $SYSCTL_CONF
+    echo "net.ipv4.conf.all.log_martians=1" >> $SYSCTL_CONF
+    echo ""
+
+    echo "Increase resiliance under heavy TCP load (SYN Flood attacks)"
+    echo "net.ipv4.tcp_max_syn_backlog=8192" >> $SYSCTL_CONF
+    echo "net.ipv4.tcp_syncookies=1" >> $SYSCTL_CONF
+    echo "net.ipv4.tcp_fin_timeout=6" >> $SYSCTL_CONF
+    echo "net.ipv4.tcp_synack_retries=2" >> $SYSCTL_CONF
+    echo ""
+
+    echo "Increase max number of sockets in TIME_WAIT state, simple DOS protection"
+    echo "net.ipv4.tcp_max_tw_buckets = 2000000" >> $SYSCTL_CONF
+
+    echo "Avoid running out of available network sockets, enabling tcp_reuse"
+    echo "net.ipv4.tcp_tw_reuse = 1" >> $SYSCTL_CONF
+
+    echo "Protect against tcp time-wait assassination hazards"
+    echo "net.ipv4.tcp_rfc1337 = 1" >> $SYSCTL_CONF
+
+    perf_tweaks
+}
+
 summary() {
     echo "This module will set system parameters which are known" 
-    echo "known to harden the TCP stack against attack"
+    echo "to harden the TCP/IP stack against attack. Network performance"
+    echo "improvements are also a part of the module. You will be given"
+    echo "an option to install network tweaks as well"
     echo "The following is a summary of the changes being made:"
     echo "   * Disable ICMP broadcast echo activity"
-    echo "       * icmp_echo_ignore_broadcast=1"
     echo "   * Disable ICMP routing redirects"
-    echo "       * accept_redirects=0"
-    echo "       * shared_media=1"
-    echo "       * secure_redirects=1"
-    echo "       * accept_redirects=0"
-    echo "       * send_redirects=0"
     echo "   * Enforce sanity checking"
-    echo "       * rp_filter=1"
     echo "   * Log and drop Martian packets"
-    echo "       * log_martians=1"
-    echo "   * Increase resiliance under heavy TCP load (SYN flood attacks)"
-    echo "       * tcp_max_syn_backlog=1200"
-    echo "       * tcp_syncookies=1"
-    echo "       * tcp_fin_timeout=3"
+    echo "   * Increase resiliance under heavy TCP load (SYN flood attacks)" 
+    echo "   * Protect against tcp time-wait assassination hazards" 
+    echo "   * Avoid running out of available network sockets, enabling tcp_reuse" 
+    echo "   * Increase max sockets in TIME_WAIT state, DOS protection" 
     echo ""
     printf "Continue with setting these parameters? (y/N): "
     read answer
 
     if [ "$answer" == "y" ] ; then
-        execute
+        sec_tweaks
     fi
 }
 
@@ -71,6 +132,7 @@ title() {
 
 main() {
     title
+    file_exists
     summary
 }
 
