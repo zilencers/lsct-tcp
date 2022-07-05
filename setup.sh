ec4dc35654c710519efa1b9fc8f4347487fa512e
zilencers <91873611+zilencers@users.noreply.github.com>
2022-07-05 18:47:39 -0500

    adjustments to formatting

--- setup.sh
+++ setup.sh
@@ -14,9 +14,9 @@ perf_tweaks() {
     read answer
 
     if [ "$answer" == "y" ] ; then
+         echo ""
          echo "Increasing the size of the receiving queue"
 	 echo "net.core.netdev_max_backlog = 16384" >> $SYSCTL_CONF
-         echo ""
 
 	 echo "Increasing performance for high speed large file transfer"
          echo "net.core.rmem_default = 1048576" >> $SYSCTL_CONF
@@ -26,24 +26,20 @@ perf_tweaks() {
          echo "net.core.optmem_max = 65536" >> $SYSCTL_CONF
          echo "net.ipv4.tcp_rmem = 4096 1048576 2097152" >> $SYSCTL_CONF
          echo "net.ipv4.tcp_wmem = 4096 65536 16777216" >> $SYSCTL_CONF
-         echo ""
 
 	 echo "Increase the default UDP limits"
          echo "net.ipv4.udp_rmem_min = 8192" >> $SYSCTL_CONF
          echo "net.ipv4.udp_wmem_min = 8192" >> $SYSCTL_CONF
-         echo ""
 
 	 echo "Enable TCP Fast Open"
 	 echo "net.ipv4.tcp_fastopen = 3" >> $SYSCTL_CONF
-	 echo ""
 
          echo "This setting kills persistent single connection performance"
 	 echo "net.ipv4.tcp_slow_start_after_idle = 0" >> $SYSCTL_CONF
-	 echo ""
 
 	 echo "Enable MTU probing, longer MTU is better for performace"
 	 echo "net.ipv4.tcp_mtu_probing = 1" >> $SYSCTL_CONF
-         echo""
+         echo ""
     fi
 
     sysctl -p
@@ -54,9 +50,9 @@ perf_tweaks() {
 }
 
 sec_tweaks() {
+    echo ""
     echo "Disabling ICMP broadcast echo activity"
     echo "net.ipv4.icmp_echo_ignore_broadcasts=1" >> $SYSCTL_CONF
-    echo ""
 
     echo "Disabling ICMP routing redirects"
     echo "net.ipv4.conf.all.accept_redirects=0" >> $SYSCTL_CONF
@@ -69,24 +65,20 @@ sec_tweaks() {
 
     #  Indicates that the media is shared with different subnets
     echo "net.ipv4.conf.all.shared_media=1" >> $SYSCTL_CONF
-    echo ""
     
     echo "Enforcing sanity checking, protect against IP spoofing"
     echo "net.ipv4.conf.default.rp_filter=1" >> $SYSCTL_CONF
     echo "net.ipv4.conf.all.rp_filter=1" >> $SYSCTL_CONF
-    echo ""
 
     echo "Logging and dropping 'Martian' packets"
     echo "net.ipv4.conf.default.log_martians=1" >> $SYSCTL_CONF
     echo "net.ipv4.conf.all.log_martians=1" >> $SYSCTL_CONF
-    echo ""
 
     echo "Increase resiliance under heavy TCP load (SYN Flood attacks)"
     echo "net.ipv4.tcp_max_syn_backlog=8192" >> $SYSCTL_CONF
     echo "net.ipv4.tcp_syncookies=1" >> $SYSCTL_CONF
     echo "net.ipv4.tcp_fin_timeout=6" >> $SYSCTL_CONF
     echo "net.ipv4.tcp_synack_retries=2" >> $SYSCTL_CONF
-    echo ""
 
     echo "Increase max number of sockets in TIME_WAIT state, simple DOS protection"
     echo "net.ipv4.tcp_max_tw_buckets = 2000000" >> $SYSCTL_CONF
@@ -96,12 +88,13 @@ sec_tweaks() {
 
     echo "Protect against tcp time-wait assassination hazards"
     echo "net.ipv4.tcp_rfc1337 = 1" >> $SYSCTL_CONF
+    echo ""
 
     perf_tweaks
 }
 
 summary() {
-    echo "This module will set system parameters which are known" 
+    echo "This module will set kernel parameters which are known" 
     echo "to harden the TCP/IP stack against attack. Network performance"
     echo "improvements are also a part of the module. You will be given"
     echo "an option to install network tweaks as well"
